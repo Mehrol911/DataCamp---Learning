534f5a7d2c123177b823c45a828ddfe1966da00b
Mehrol911 <80707348+Mehrol911@users.noreply.github.com>
2022-05-07 10:04:22 +0500

    Update Filtering rows.sql

--- SQL/Filtering rows.sql
+++ SQL/Filtering rows.sql
@@ -1,5 +1,5 @@
 SELECT title, release_year
 FROM films
-WHERE release_year BETWEEN 1990 AND 2000
+WHERE (release_year BETWEEN 1990 AND 2000
 AND budget > 100000000
-AND language = 'Spanish' OR language = 'French';
+AND language = 'Spanish' OR language = 'French');
