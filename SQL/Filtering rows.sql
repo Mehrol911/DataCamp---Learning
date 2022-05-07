c4a57d496ed0d36c836d71f36e0dab90236252e4
Mehrol911 <80707348+Mehrol911@users.noreply.github.com>
2022-05-07 10:33:59 +0500

    Update Filtering rows.sql

--- SQL/Filtering rows.sql
+++ SQL/Filtering rows.sql
@@ -1,5 +1,24 @@
+/*USING BETWEEN */
 SELECT title, release_year
 FROM films
-WHERE (release_year BETWEEN 1990 AND 2000
+WHERE release_year BETWEEN 1990 AND 2000
 AND budget > 100000000
-AND language = 'Spanish' OR language = 'French');
+AND (language = 'Spanish' OR language = 'French');
+
+/*WHERE IN
+As you've seen, WHERE is very useful for filtering results. However, 
+if you want to filter based on many conditions, 
+WHERE can get unwieldy. For example:*/
+
+SELECT title, release_year
+FROM films
+WHERE release_year in (1990, 2000)
+AND duration > 120;
+
+SELECT title, language
+FROM films
+WHERE language IN ('English', 'Spanish', 'French');
+
+SELECT title, certification
+FROM films
+WHERE certification IN ('NC-17', 'R')
